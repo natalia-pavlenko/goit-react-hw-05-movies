ff7f1e6cf8985c72adde644cb8bdbf16e4c41681
natalia-pavlenko <106035205+natalia-pavlenko@users.noreply.github.com>
2023-03-15 18:50:23 +0000

    Deploying to gh-pages from @ natalia-pavlenko/goit-react-hw-05-movies@9fd67be8fabce264364546f4e4041b19819858e2 🚀

--- static/js/500.3ff46ba4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[500],{500:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"ReviewsPage"})}}}]);
-//# sourceMappingURL=500.3ff46ba4.chunk.js.map
